b62473208897d358f3fdd47f58106d78e8b8261b
Pieterjan C <pieterjan.carly@gmail.com>
2017-01-13 15:20:22 +0100

    Changed to entity_presave_hook

---
+++ src/FieldProcessor/Processor.php
@@ -0,0 +1,134 @@
+<?php
+namespace Drupal\autonumber\FieldProcessor;
+
+use \Drupal\field\Entity\FieldConfig;
+use \Drupal\Core\Entity\ContentEntityBase;
+
+
+class Processor
+{
+  private $entity;
+  private $fieldDefinition;
+
+  public function __construct(ContentEntityBase $entity, FieldConfig $fieldDefinition)
+  {
+    $this->entity = $entity;
+    $this->fieldDefinition = $fieldDefinition;
+  }
+
+  public function getFieldName()
+  {
+    return $this->fieldDefinition->getName();
+  }
+
+  public function getEntityType()
+  {
+    return $this->entity->getEntityTypeId();
+  }
+
+  public function getEntity()
+  {
+    return $this->entity;
+  }
+
+  public function getSetting($setting)
+  {
+    return $this->fieldDefinition->getSetting($setting);
+  }
+
+  public function process()
+  {
+    if($this->shouldUpdateValue())
+    {
+      $field = $this->getFieldName();
+
+      $entity = $this->getEntity();
+      $groupingvalue = $this->getGroupingValue();
+      $nextValue = $this->getNextValueForGrouping($groupingvalue);
+
+      $entity->$field->value = $nextValue;
+      $entity->$field->auto_grouping_pattern = $this->getSetting('auto_grouping_pattern');
+      $entity->$field->auto_grouping = $this->getGroupingValue();
+    }
+    else
+    {
+      $this->restoreOldValues();
+    }
+  }
+
+  private function shouldUpdateValue()
+  {
+    $entity = $this->getEntity();
+    $field = $this->getFieldName();
+    return empty($entity->original) || empty($entity->original->$field->value);
+  }
+
+  private function restoreOldValues()
+  {
+    $entity = $this->getEntity();
+    $field = $this->getFieldName();
+
+    $entity->$field->value = $entity->original->$field->value;
+    $entity->$field->auto_grouping_pattern = $entity->original->$field->auto_grouping_pattern;
+    $entity->$field->auto_grouping = $entity->original->$field->auto_grouping;
+  }
+
+  private function getGroupingValue()
+  {
+    $entity = $this->getEntity();
+    $year = date('Y', $entity->created->value);
+    $quarter = $this->getQuarter($entity->created->value);
+    $month = date('m', $entity->created->value);
+    $day = date('d', $entity->created->value);
+
+    $groupingPattern = $this->getSetting('auto_grouping_pattern');
+
+    $groupingValue = '';
+    $groupingValue .= strstr($groupingPattern, 'YYYY') ? $year : 'YYYY';
+    $groupingValue .= '-';
+    $groupingValue .= strstr($groupingPattern, 'QQ') ? $quarter : 'QQ';
+    $groupingValue .= '-';
+    $groupingValue .= strstr($groupingPattern, 'MM') ? $month : 'MM';
+    $groupingValue .= '-';
+    $groupingValue .= strstr($groupingPattern, 'DD') ? $day : 'DD';
+
+    return $groupingValue;
+  }
+
+  private function getQuarter($date)
+  {
+    return ceil(date('n', $date)/3);
+  }
+
+  private function getNextValueForGrouping($autoGrouping = null, $manualGrouping = null)
+  {
+    // TODO: find a way to handle this thread safe.
+    $entityType = $this->getEntityType();
+    $fieldName = $this->getFieldName();
+
+    $query = \Drupal::database()->select($entityType.'__'.$fieldName, 'field');
+    $query->addExpression('MAX('.$fieldName.'_value)', 'maximumvalue');
+
+    if(!empty($autoGrouping))
+    {
+      $query->condition('field.'.$fieldName.'_auto_grouping', $autoGrouping);
+    }
+    if(!empty($manualGrouping))
+    {
+      $query->condition('field.'.$fieldName.'_manual_grouping', $manualGrouping);
+    }
+
+    $currentMaxValue = $query->execute()->fetchField();
+
+    if(empty($currentMaxValue))
+    {
+      $currentMaxValue = 0;
+    }
+
+    // Lets make sure the value is handled as an Integer
+    $currentMaxValue = (int) $currentMaxValue;
+
+    // We increase it with 1
+    return ++$currentMaxValue;
+  }
+}
--- src/Plugin/Field/FieldType/AutonumberItem.php
+++ src/Plugin/Field/FieldType/AutonumberItem.php
@@ -40,6 +40,7 @@ class AutonumberItem extends FieldItemBase implements FieldItemInterface
    */
   public static function schema(FieldStorageDefinitionInterface $field_definition)
   {
+    // dump('2');die;
     return [
       'columns' => [
         'auto_grouping_pattern' => [
@@ -72,115 +73,14 @@ class AutonumberItem extends FieldItemBase implements FieldItemInterface
    */
   public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition)
   {
+    // dump('3');die;
     $properties = [];
     $properties['auto_grouping_pattern'] = DataDefinition::create('string')->setLabel(t('The pattern used for creating the auto grouping.'));
     $properties['auto_grouping'] = DataDefinition::create('string')->setLabel(t('The auto grouping.'));
     $properties['manual_grouping'] = DataDefinition::create('string')->setLabel(t('An extra grouping level that can be set via code.'));
-    $properties['created'] = DataDefinition::create('string')->setLabel(t('The timestamp this value was created, the auto grouping is patterns are based off of this value.'));
-    $properties['value'] = DataDefinition::create('string')->setLabel(t('The generated number.'));
+    $properties['value'] = DataDefinition::create('integer')->setLabel(t('The generated number.'))->setComputed(TRUE)->setRequired(TRUE);
 
     return $properties;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function preSave()
-  {
-    if($this->shouldUpdateValue())
-    {
-      $field = $this->getParent()->getName();
-
-      $entity = $this->getEntity();
-      $groupingvalue = $this->getGroupingValue();
-      $nextValue = $this->getNextValueForGrouping($groupingvalue);
-
-      $entity->$field->value = $nextValue;
-      $entity->$field->auto_grouping_pattern = $this->getSetting('auto_grouping_pattern');
-      $entity->$field->auto_grouping = $this->getGroupingValue();
-    }
-    else
-    {
-      $this->restoreOldValues();
-    }
-  }
-
-  private function shouldUpdateValue()
-  {
-    $entity = $this->getEntity();
-    $field = $this->getParent()->getName();
-    return empty($entity->original) || empty($entity->original->$field->value);
-  }
-
-  private function restoreOldValues()
-  {
-    $entity = $this->getEntity();
-    $field = $this->getParent()->getName();
-    
-    $entity->$field->value = $entity->original->$field->value;
-    $entity->$field->auto_grouping_pattern = $entity->original->$field->auto_grouping_pattern;
-    $entity->$field->auto_grouping = $entity->original->$field->auto_grouping_pattern;
-  }
-
-  private function getGroupingValue()
-  {
-    $entity = $this->getEntity();
-    $year = date('Y', $entity->created->value);
-    $quarter = $this->getQuarter($entity->created->value);
-    $month = date('m', $entity->created->value);
-    $day = date('d', $entity->created->value);
-
-    $groupingPattern = $this->getSetting('auto_grouping_pattern');
-
-    $groupingValue = '';
-    $groupingValue .= strstr($groupingPattern, 'YYYY') ? $year : 'YYYY';
-    $groupingValue .= '-';
-    $groupingValue .= strstr($groupingPattern, 'QQ') ? $quarter : 'QQ';
-    $groupingValue .= '-';
-    $groupingValue .= strstr($groupingPattern, 'MM') ? $month : 'MM';
-    $groupingValue .= '-';
-    $groupingValue .= strstr($groupingPattern, 'DD') ? $day : 'DD';
-
-    return $groupingValue;
-  }
-
-  private function getQuarter($date)
-  {
-    return ceil(date('n', $date)/3);
-  }
-
-  private function getNextValueForGrouping($autoGrouping = null, $manualGrouping = null)
-  {
-    // TODO: find a way to handle this thread safe.
-    $definition = $this->getFieldDefinition();
-
-    $entity = $definition->getTargetEntityTypeId();
-    $fieldName = $definition->getName();
-
-    $query = \Drupal::database()->select($entity.'__'.$fieldName, 'field');
-    $query->addExpression('MAX('.$fieldName.'_value)', 'maximumvalue');
-
-    if(!empty($autoGrouping))
-    {
-      $query->condition('field.'.$fieldName.'_auto_grouping', $autoGrouping);
-    }
-    if(!empty($manualGrouping))
-    {
-      $query->condition('field.'.$fieldName.'_manual_grouping', $manualGrouping);
-    }
-
-    $currentMaxValue = $query->execute()->fetchField();
-
-    if(empty($currentMaxValue))
-    {
-      $currentMaxValue = 0;
-    }
-
-    // Lets make sure the value is handled as an Integer
-    $currentMaxValue = (int) $currentMaxValue;
-
-    // We increase it with 1
-    return ++$currentMaxValue;
   }
 
   /**
@@ -198,14 +98,5 @@ class AutonumberItem extends FieldItemBase implements FieldItemInterface
     );
 
     return $element;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function isEmpty()
-  {
-    $value = $this->get('value')->getValue();
-    return $value === NULL || $value === '';
   }
 }
